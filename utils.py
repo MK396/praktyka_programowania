b5944dd15a72c93a5b716a136885c015fb0b318e
Mikołaj Kużownik <272552@student.pwr.edu.pl>
2025-05-20 14:17:45 +0200

    commit2

--- utils.py
+++ utils.py
@@ -2,28 +2,35 @@
 Kalkulator
 """
 
-def add ( a: int , b: int) -> int:
+
+def add(a: int, b: int) -> int:
     """
     :param a:
     :param b:
     :return:
     """
     return a + b
-def subtract ( a: int , b: int) -> int:
+
+
+def subtract(a: int, b: int) -> int:
     """
     :param a:
     :param b:
     :return:
     """
     return a - b
-def multiply ( a: int , b: int) -> int:
+
+
+def multiply(a: int, b: int) -> int:
     """
     :param a:
     :param b:
     :return:
     """
     return a * b
-def divide ( a: int , b: int) -> float :
+
+
+def divide(a: int, b: int) -> float:
     """
     :param a:
     :param b:
